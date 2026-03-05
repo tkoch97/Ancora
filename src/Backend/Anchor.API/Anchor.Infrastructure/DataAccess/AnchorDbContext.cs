e146381f6042dd75b011b3f9d0c9ffe1f5c5d281
tkoch97 <tkoch.dev@gmail.com>
2026-03-05 20:50:17 -0300

    feat: configure DbContext

---
+++ src/Backend/Anchor.API/Anchor.Infrastructure/DataAccess/AnchorDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Anchor.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anchor.Infrastructure.DataAccess
+{
+    public class AnchorDbContext : DbContext
+    {
+        public AnchorDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Emotion> Emotions { get; set; }
+        public DbSet<ScriptureReference> ScriptureReferences { get; set; }
+        public DbSet<EmotionContent> EmotionContents { get; set; }
+        public DbSet<EmotionScripture> EmotionScriptures { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            modelBuilder.Entity<EmotionScripture>()
+                .HasKey(es => new { es.EmotionId, es.ScriptureReferenceId });
+
+            modelBuilder.Entity<ScriptureReference>()
+                .HasIndex(s => new { s.Book, s.Chapter, s.VerseStart, s.VerseEnd }).IsUnique();
+
+            modelBuilder.Entity<Emotion>()
+                .HasOne(e => e.EmotionContent)
+                .WithOne(ec => ec.Emotion)
+                .HasForeignKey<EmotionContent>(ec => ec.EmotionId);
+
+            modelBuilder.Entity<EmotionScripture>()
+                .HasOne(es => es.Emotion) // Cada linha da tabela vai apontar apenas para uma emoção
+                .WithMany(e => e.EmotionScriptures) // Uma emoção pode aparecer em várias linhas da tabela EmotionScriptures
+                .HasForeignKey(es => es.EmotionId);
+
+            modelBuilder.Entity<EmotionScripture>()
+                .HasOne(es => es.ScriptureReference)
+                .WithMany(e => e.EmotionScriptures)
+                .HasForeignKey(es => es.ScriptureReferenceId);
+        }
+    }
+}
